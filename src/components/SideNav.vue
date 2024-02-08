69d1cbafb906be4ed8cc1410d15789a38e45820a
fobabett <te.vallee@gmail.com@users.noreply.github.com>
2024-02-07 17:48:55 -0800

    fixesroutes

--- src/components/SideNav.vue
+++ src/components/SideNav.vue
@@ -6,19 +6,19 @@
 <template>
   <v-navigation-drawer theme="dark" permanent>
     <v-list color="transparent">
-      <v-list-item prepend-icon="mdi-home" title="Home" @click="router.push(`/`)"></v-list-item>
-      <v-list-item prepend-icon="mdi-trending-up" title="Trending" @click="router.push(`/`)"></v-list-item>
+      <v-list-item prepend-icon="mdi-home" title="Home" @click="router.push(`/FeedbackForum/`)"></v-list-item>
+      <v-list-item prepend-icon="mdi-trending-up" title="Trending" @click="router.push(`/FeedbackForum/`)"></v-list-item>
       <v-divider></v-divider>
       <v-list-item title="Genres"></v-list-item>
-      <v-list-item prepend-icon="mdi-music" title="EDM" @click="router.push(`/?=EDM`)"></v-list-item>
-      <v-list-item prepend-icon="mdi-music" title="Hip Hop" @click="router.push(`/?=Hip Hop`)"></v-list-item>
-      <v-list-item prepend-icon="mdi-music" title="Hip Hop" @click="router.push(`/?=Hip Hop`)"></v-list-item>
-      <v-list-item prepend-icon="mdi-music" title="Jazz" @click="router.push(`/?=Jazz`)"></v-list-item>
-      <v-list-item prepend-icon="mdi-music" title="R&B" @click="router.push(`/?=R&B`)"></v-list-item>
-      <v-list-item prepend-icon="mdi-music" title="Orchestral" @click="router.push(`/?=Orchestral`)"></v-list-item>
-      <v-list-item prepend-icon="mdi-music" title="8-Bit" @click="router.push(`/?=8-Bit`)"></v-list-item>
-      <v-list-item prepend-icon="mdi-music" title="Rock" @click="router.push(`/?=Rock`)"></v-list-item>
-      <v-list-item prepend-icon="mdi-music" title="Metal" @click="router.push(`/?=Metal`)"></v-list-item>
+      <v-list-item prepend-icon="mdi-music" title="EDM" @click="router.push(`/FeedbackForum/?=EDM`)"></v-list-item>
+      <v-list-item prepend-icon="mdi-music" title="Hip Hop" @click="router.push(`/FeedbackForum/?=Hip Hop`)"></v-list-item>
+      <v-list-item prepend-icon="mdi-music" title="Hip Hop" @click="router.push(`/FeedbackForum/?=Hip Hop`)"></v-list-item>
+      <v-list-item prepend-icon="mdi-music" title="Jazz" @click="router.push(`/FeedbackForum/?=Jazz`)"></v-list-item>
+      <v-list-item prepend-icon="mdi-music" title="R&B" @click="router.push(`/FeedbackForum/?=R&B`)"></v-list-item>
+      <v-list-item prepend-icon="mdi-music" title="Orchestral" @click="router.push(`/FeedbackForum/?=Orchestral`)"></v-list-item>
+      <v-list-item prepend-icon="mdi-music" title="8-Bit" @click="router.push(`/FeedbackForum/?=8-Bit`)"></v-list-item>
+      <v-list-item prepend-icon="mdi-music" title="Rock" @click="router.push(`/FeedbackForum/?=Rock`)"></v-list-item>
+      <v-list-item prepend-icon="mdi-music" title="Metal" @click="router.push(`/FeedbackForum/?=Metal`)"></v-list-item>
     </v-list>
     <!-- <v-list color="transparent" v-for="genre in genres" :key="genre">
       <v-list-item :title="genere" @click="router.push(`/?=${genere}`)"></v-list-item>
